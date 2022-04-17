d36b455944f4317d9a51b65d2ff5d2d416404f92
Programming-School <mohamedehabam99@hotmail.com>
2022-04-17 12:52:48 +0200

    New Project Structure

--- Theme toggler/theme.js
+++
@@ -1,9 +0,0 @@
-/*just adding event listener for toggle container*/
-
-document.querySelector(".toggle-container").addEventListener("click",function name(){
-/*things that should be executed when eventlistner is being fired*/  
-/*adding darkmode to circle class in order to change its position*/  
-document.querySelector(".circle").classList.toggle("darkmode");
-/*adding the darkmode to body element to change the color of everything*/
-document.body.classList.toggle("darkmode");
-});
