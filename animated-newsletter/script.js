2a6fd8a30d87d8214af15a8a9342ce0b6a22c435
Swati A Firangi <swatifirangi29@gmail.com>
2022-10-05 16:39:53 +0530

    add comments

--- animated-newsletter/script.js
+++ animated-newsletter/script.js
@@ -1,8 +1,13 @@
+/*ready() function prevents any jQuery code from running before the document is finished loading*/
 $(document).ready(function () {
+	/*selects 'envelope' class on 'click' action*/
 	$(".envelope").click(function () {
+		/*selects 'overlay' class to toggle between adding and removing 'active' class*/
 		$(".overlay").toggleClass("active");
 	});
+	/*select 'close' class on 'click' action*/
 	$(".close").click(function () {
+		/*selects 'overlay' class to toggle between adding and removing 'active' class*/
 		$(".overlay").toggleClass("active");
 	});
 });
