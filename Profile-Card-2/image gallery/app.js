b0a081a5d839884fcf9b7c8ac160274c808329db
hamzailyas237 <hamzailyas237@gmail.com>
2022-10-06 21:38:53 +0500

    Image Gallery Template Added

---
+++ Profile-Card-2/image gallery/app.js
@@ -0,0 +1,7 @@
+function imageGallery(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+  }
